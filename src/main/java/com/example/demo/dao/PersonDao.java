dad3f8cd81b854d00d2570ab7188b968ec74023d
Taiwo Farinu <farinupeter@gmail.com>
2021-08-23 11:26:05 +0100

    first commit

---
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Taiwo Farinu on 03-Feb-21
+ */
+
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+}
